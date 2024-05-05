280ab1d18bc02d3856f191b3f5bf99e52f1bdc1e
1 <2>
2024-05-05 15:06:22 +0500

    First commit

---
+++ src/UserService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Microservices;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Http;
+
+class UserService
+{
+    private $endpoint;
+
+    public function __construct() {
+        $this->endpoint = env('USER_ENDPOINT');
+    }
+
+    public function headers()
+    {
+        return [
+            'Authorization' => request()->headers->get('Authorization'),
+        ];
+    }
+
+    public function request()
+    {
+        return Http::withHeaders($this->headers());
+    }
+
+    public function getUser()
+    {
+        $json = $this->request()->get("{$this->endpoint}/user")->json();
+
+        return new User($json);
+    }
+
+    public function isAdmin()
+    {
+        return $this->request()->get("{$this->endpoint}/admin")->successful();
+    }
+
+    public function isInfluencer()
+    {
+        return $this->request()->get("{$this->endpoint}/influencer")->successful();
+    }
+
+    public function allows($ability, $arguments)
+    {
+        Gate::forUser($this->getUser())->authorize($ability,$arguments);
+    }
+
+    public function all($page)
+    {
+        return $this->request()->get("{$this->endpoint}/users?page={$page}")->json();
+    }
+
+    public function get($id):User
+    {
+        $json = $this->request()->get("{$this->endpoint}/users/{$id}")->json();
+
+        return new User($json);
+    }
+
+    public function create($data)
+    {
+        $json = $this->request()->post("{$this->endpoint}/users", $data)->json();
+
+        return new User($json);
+    }
+
+    public function update($id, $data)
+    {
+        $json = $this->request()->put("{$this->endpoint}/users/{$id}", $data)->json();
+
+        return new User($json);
+    }
+
+    public function delete($id)
+    {
+        return $this->request()->delete("{$this->endpoint}/users/{$id}")->successful();
+    }
+}
